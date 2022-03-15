014cf49f87010edcc76276ec8de5ff7d936aa592
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-03-15 22:20:10 +0100

    Completed 'index.html.erb' + related controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
+
+  # JBV 15.03.2022 - TO DO: Discuss with the team if a "devise" authentication would be needed here?
+
+  # JBV 15.03.2022 - Created 'index' method to be able to complete the routing to and feed of the "All products" page
   def index
+    @items = Item.all
   end
+
 end
