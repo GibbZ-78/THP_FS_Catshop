e6c0e3024f9bcf5ce6b9c1e6c096be8ba357e120
WilfriedPaillot <paillotwilfried@gmail.com>
2022-03-16 18:45:48 +0100

    Update navbar & footer cart link + controller index + update jbv method in cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,23 +1,35 @@
 class CartsController < ApplicationController
-  # before_action :authenticate_user!
-  
+  before_action :authenticate_user!
+
   def index
+    # Le show du cart doit renvoyer les cart_items
     
   end
 
-  def show
-    # Le show du cart doit renvoyer les cart_items
-  end
+  # def show
+  # end
 
   def new
     # Instancie l'objet cart
+
   end
 
   def update
-    # Met à jour le contenu de l'objet cart - cf. les cart items et le montant total
+  # JBV 16.03.2022 - Instantiated the "update" method of the current_user's cart when adding an item directly from a czrd of the 'items/index'
+  # NB: if NO cart exists already for the current user (e.g. very first connexion) then create one
+    if user_signed_in?
+      @cart = Cart.find_by(user_id:current_user.id)   # At this stage, the user could have a cart already (or NOT!)
+      if @cart.nil?                                  # If the user has a cart already, @cart points to it.
+        @cart = Cart.create(user_id:current_user.id, comment: "Cart created to enable adding a new item")
+      end
+      @cart_item = CartItem.create(cart_id:@cart.id, item_id:params[:cat_id], comment:"Added from 'add to cart' button")
+    else
+      redirect_to user_new_session_path     # If user NOT signed-in but, for whatever reason, lands here (not possible theoretically), then redirect to web home page
+    end
   end
 
   def destroy
     # Supprime le contenu de l'objet cart - 1..N items_cart
+
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
 
   def index
     @items = Item.all
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(post_params)
+    redirect_to item_path(item.id)
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(post_params)
+    redirect_to items_path
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+
+  def post_params
+    params.require(:item).permit(:title, :description, :price, :image_url)
+  end
 end
