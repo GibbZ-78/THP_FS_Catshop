063c482eef8393eca29fbfe3fd5b3701b2f86939
WilfriedPaillot <paillotwilfried@gmail.com>
2022-03-16 17:00:11 +0100

    Added fixed-bottom to footer

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,6 @@
 class ItemsController < ApplicationController
-
-  # JBV 15.03.2022 - TO DO: Discuss with the team if a "devise" authentication would be needed here?
   before_action :authenticate_user!, except: [:index]
 
-  # JBV 15.03.2022 - Created 'index' method to be able to complete the routing to and feed of the "All products" page
   def index
     @items = Item.all
   end
