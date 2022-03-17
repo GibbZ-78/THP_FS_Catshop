5694353a1652cb10acb3405700fa2d8e0e9c15f0
WilfriedPaillot <paillotwilfried@gmail.com>
2022-03-17 02:03:09 +0100

    Updated carts controller update & index + Added quantity in index view + Added quantity column in cart_items + updated hero_banner

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,12 +2,12 @@ class CartsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # Le show du cart doit renvoyer les cart_items
     @cart = Cart.find_by(user_id:current_user.id)
     if @cart.nil?
       @cart = Cart.create(user_id:current_user.id, comment: "Cart created")
-      @items = @cart.items
+      @items = @cart.items  # price    
     else
+      @cart_items = CartItem.where(cart_id:@cart.id) # quantity  
       @items = @cart.items
     end
   end
@@ -16,8 +16,6 @@ class CartsController < ApplicationController
   # end
 
   # def new
-  #   # Instancie l'objet cart
-
   # end
 
   def update
@@ -25,10 +23,17 @@ class CartsController < ApplicationController
   # NB: if NO cart exists already for the current user (e.g. very first connexion) then create one
     if user_signed_in?
       @cart = Cart.find_by(user_id:current_user.id)   # At this stage, the user could have a cart already (or NOT!)
-      if @cart.nil?                                  # If the user has a cart already, @cart points to it.
+      if @cart.nil?                                   # If the user has a cart already, @cart points to it.
         @cart = Cart.create(user_id:current_user.id, comment: "Cart created to enable adding a new item")
       end
-      @cart_item = CartItem.create(cart_id:@cart.id, item_id:params[:id], comment:"Added from 'add to cart' button")
+      
+      @cart_item = CartItem.find_by(cart_id:@cart.id, item_id:params[:id])
+      if @cart_item.nil?
+        @cart_item = CartItem.create(cart_id:@cart.id, item_id:params[:id], quantity:1, comment:"Added from 'add to cart' button")
+      else      
+        @cart_item.update(quantity: (@cart_item.quantity+1), comment:"Added 1 to quantity")
+      end
+    
     else
       redirect_to user_new_session_path     # If user NOT signed-in but, for whatever reason, lands here (not possible theoretically), then redirect to web home page
     end
@@ -36,10 +41,6 @@ class CartsController < ApplicationController
 
   def destroy
     # Supprime le contenu de l'objet cart - 1..N items_cart
-
   end
-
-
-
 
 end
